fc613db33993fdf8fa742ae4306a2545d7f619d8
Yakshit Poojary <yakshitpoojary2004@gmail.com>
2022-11-09 10:29:21 +0530

    Update CAR WASH KWEEWEE PROG.c

--- CAR WASH KWEEWEE PROG.c
+++ CAR WASH KWEEWEE PROG.c
@@ -60,7 +60,7 @@ void start_time(struct node* ptr)
         start_time= start_time + ptr->time;
         ptr=ptr->next;
     }
-    printf("The start time for %d is %d",ptr->lic,start_time);
+    printf("The start time for %d is %d\n",ptr->lic,start_time);
 }
 
 void end_time(struct node * ptr)
@@ -72,7 +72,7 @@ void end_time(struct node * ptr)
         ptr=ptr->next;
     }
     end_time= end_time + ptr->time;
-    printf("The end time for %d is %d",ptr->lic,end_time);
+    printf("The end time for %d is %d\n",ptr->lic,end_time);
 }
 
 void cars_ahead(struct node * ptr)
@@ -83,25 +83,62 @@ void cars_ahead(struct node * ptr)
         count = count + 1;
         ptr = ptr->next;
     }
-    printf("Number of cars ahead of %d is %d",ptr->lic,count);
+    printf("Number of cars ahead of %d is %d\n",ptr->lic,count);
 }
 
-void start_id(struct node* Front,struct node* ptr, int ID)
+void start_id(struct node* Front,int ID)
 {
     struct node* temp=(struct node*)malloc(sizeof(struct node));
     temp=Front;
+    struct node* ptr=(struct node*)malloc(sizeof(struct node));
+    ptr=Front;
     int flag=0;
+    int start_time=0;
     do
     {
         if(temp->lic==ID)
         {
-            int start_time=0;
-            while(ptr->next!=NULL)
+            
+            while(ptr->lic!=temp->lic)
             {
-                start_time= start_time + ptr->time;
+                if(ptr->wax==1)
+                {
+                    start_time= start_time + ptr->time;
+                    ptr=ptr->next;
+                }
+                else if(ptr->wax==0)
+                {
+                    start_time=start_time+10;
+                    ptr=ptr->next;
+                }
+            }
+            printf("The start time for %d is %d\n",ptr->lic,start_time);
+            flag=1;
+        }
+        else
+        {
+            temp=temp->next;
+        }
+    }while(flag!=1);
+}
+
+void end_id(struct node* Front,struct node* ptr, int ID)
+{
+    struct node* temp=(struct node*)malloc(sizeof(struct node));
+    temp=Front;
+    int flag=0;
+    int end_time=0;
+    do
+    {
+        if(temp->lic==ID)
+        {
+            while(ptr->lic!=temp->lic)
+            {
+                end_time= end_time + ptr->time;
                 ptr=ptr->next;
             }
-            printf("The start time for %d is %d",ptr->lic,start_time);
+            end_time= end_time + ptr->time;
+            printf("The end time for %d is %d\n",ptr->lic,end_time);
             flag=1;
         }
         else
@@ -121,11 +158,12 @@ int main()
     Front=insert(Front, 1234, 1, 14);
     Front=insert(Front, 5678, 0, 10);
     Front=insert(Front, 911, 1, 14);
-    Front=insert(Front, 6789, 1, 10);
+    Front=insert(Front, 6789,0 , 10);
     Front=insert(Front, 666, 1, 14);
     display(Front);
     start_time(Front);
     end_time(Front);
     cars_ahead(Front);
-    start_id(Front,Front,6789);
+    start_id(Front,6789);
+    end_id(Front,Front,6789);
 }
